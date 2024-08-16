66baf0a56a5a8291ed673e4ef9847e1ca6e4951b
Simon Legner <Simon.Legner@gmail.com>
2024-08-16 08:34:17 +0200

    Fix code style

--- src/components/TemplatesTab.vue
+++ src/components/TemplatesTab.vue
@@ -16,37 +16,37 @@
       </div>
     </div>
 
-<div class="field-row">
-  <div v-for="option in options">
-	<cdx-field :label-icon="cdxIconLink">
-	  <cdx-text-area
-		name="templates"
-		v-model="state[`outlinks_${option}`]"
-	  />
-	  <template #label>{{ tt("outlinks") }}</template>
-	  <template #description>{{ tt(`outlinks_${option}`) }}</template>
-	</cdx-field>
-  </div>
-</div>
+    <div class="field-row">
+      <div v-for="option in options">
+        <cdx-field :label-icon="cdxIconLink">
+          <cdx-text-area
+            name="templates"
+            v-model="state[`outlinks_${option}`]"
+          />
+          <template #label>{{ tt("outlinks") }}</template>
+          <template #description>{{ tt(`outlinks_${option}`) }}</template>
+        </cdx-field>
+      </div>
+    </div>
 
-<div class="field-row">
-  <div v-for="option in options">
-	<cdx-field :label-icon="cdxIconLink">
-	  <cdx-text-area
-		name="templates"
-		v-model="state[`links_to_${option === 'yes' ? 'any' : option}`]"
-	  />
-	  <template #label>{{ tt("links_to") }}</template>
-	  <template #description>{{ tt(`outlinks_${option}`) }}</template>
-	</cdx-field>
-  </div>
-</div>
+    <div class="field-row">
+      <div v-for="option in options">
+        <cdx-field :label-icon="cdxIconLink">
+          <cdx-text-area
+            name="templates"
+            v-model="state[`links_to_${option === 'yes' ? 'any' : option}`]"
+          />
+          <template #label>{{ tt("links_to") }}</template>
+          <template #description>{{ tt(`outlinks_${option}`) }}</template>
+        </cdx-field>
+      </div>
+    </div>
   </section>
 </template>
 
 <script setup lang="ts">
 import { CdxCheckbox, CdxField, CdxTextArea } from "@wikimedia/codex";
-import { cdxIconLink, cdxIconTemplateAdd } from '@wikimedia/codex-icons';
+import { cdxIconLink, cdxIconTemplateAdd } from "@wikimedia/codex-icons";
 import tt from "../i18n/tt";
 import { useState } from "../useState";
 
